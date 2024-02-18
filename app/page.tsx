4c056141b77acc147fb4e73a4a21dcde60b1e203
fauzibahar <fauzibahar74@gmail.com>
2024-02-18 13:23:10 +0700

    feat: Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,13 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  const handleClick = () => {
-    router.push("/contacts");
-  };
 
   return (
     <div className="flex justify-center bg-teal-600 min-h-screen items-center">
       <button
         type="button"
         className="rounded-lg border text-xl p-2 bg-white hover:drop-shadow-2xl text-teal-600"
-        onClick={handleClick}
+        onClick={() => router.push("/contacts")}
       >
         Get Started
       </button>
